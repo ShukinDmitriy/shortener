caf74beb7bd88d12352b05b299f1998c383c79f8
shchukin <dvshhukin@mts.ru>
2024-02-03 23:36:00 +0300

    iter1 Shortener server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,86 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type URLShortener struct {
+	urls map[string]string
+}
+
+func generateShortKey() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const keyLength = 6
+
+	rand.Seed(time.Now().UnixNano())
+	shortKey := make([]byte, keyLength)
+	for i := range shortKey {
+		shortKey[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(shortKey)
+}
+
+func (us *URLShortener) HandleShorten(res http.ResponseWriter, req *http.Request) {
+	originalURL, err := io.ReadAll(req.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Generate a unique shortened key for the original URL
+	shortKey := generateShortKey()
+	us.urls[shortKey] = string(originalURL)
+
+	res.WriteHeader(http.StatusCreated)
+	// TODO как получить текущий протокол из запроса?
+	res.Write([]byte("http://" + req.Host + "/" + shortKey))
+}
+
+func (us *URLShortener) HandleRedirect(res http.ResponseWriter, req *http.Request) {
+	shortKey := req.URL.Path[len("/"):]
+
+	if shortKey == "" {
+		http.Error(res, "", http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the original URL from the `urls` map using the shortened key
+	originalURL, found := us.urls[shortKey]
+	if !found {
+		http.Error(res, "", http.StatusBadRequest)
+	}
+
+	http.Redirect(res, req, originalURL, http.StatusTemporaryRedirect)
+}
+
+var shortener = &URLShortener{
+	urls: make(map[string]string),
+}
+
+func mainHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		shortener.HandleRedirect(res, req)
+		return
+	}
+
+	if req.Method == http.MethodPost {
+		shortener.HandleShorten(res, req)
+		return
+	}
+
+	http.Error(res, "", http.StatusBadRequest)
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, mainHandler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
