8521f806ce96ba959cb5d4a4669793825f248b35
shchukin <dvshhukin@mts.ru>
2024-02-03 23:56:38 +0300

    iter1 fix rand.Seed

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -15,10 +15,11 @@ func generateShortKey() string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	const keyLength = 6
 
-	rand.Seed(time.Now().UnixNano())
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
 	shortKey := make([]byte, keyLength)
 	for i := range shortKey {
-		shortKey[i] = charset[rand.Intn(len(charset))]
+		shortKey[i] = charset[rng.Intn(len(charset))]
 	}
 	return string(shortKey)
 }
