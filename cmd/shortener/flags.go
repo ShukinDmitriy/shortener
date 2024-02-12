fcd7c1c8ad7dfc46f7c4acd9509b5dcafd390546
shchukin <dvshhukin@mts.ru>
2024-02-12 20:46:39 +0300

    iter4 change host

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -15,7 +15,7 @@ var flagBaseAddr string
 func parseFlags() {
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
-	flag.StringVar(&flagRunAddr, "a", "http://127.0.0.1:8080", "address and port to run server")
+	flag.StringVar(&flagRunAddr, "a", "127.0.0.1:8080", "address and port to run server")
 
 	// регистрируем переменную flagBaseAddr
 	// как аргумент -b со значением :8000 по умолчанию
